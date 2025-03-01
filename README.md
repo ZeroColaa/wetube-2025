b057c15687753d00e99b73d2d36894ccb3d224af
ZeroColaa <sinyoung001128@gmail.com>
2025-03-01 22:01:28 +0900

    routers
    routers practice

---
+++ README.md
@@ -0,0 +1,16 @@
+# wetube-2025
+
+/ -> Home
+/join -> Join
+/login -> Login
+/serach -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/delete -> Delete My user
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -1,43 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4590;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  //middleware
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue");
-  next();
-};
-const handleHome = (req, res) => {
-  //finalware
-  return res.send("I love middlewares");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("welcome to the private lounge.");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-//반드시 function을 send 해줘야 한다.
-app.get("/", handleHome);
-//button.addEventListener("click ",handleClick)와 같다
-app.get("/protected", handleProtected);
 
-const handleLogin = (req, res) => {
-  return res.send("log in here");
-};
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
